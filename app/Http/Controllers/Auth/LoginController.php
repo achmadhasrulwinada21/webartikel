08fd957969da0ab32d6783da861f40d45724b8fc
nugrohotanjo <rizkyarchery@gmail.com>
2019-11-11 17:24:54 +0700

    tambahin redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,6 +20,10 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function authenticated() {
+        return redirect('/management');
+    }
+
     /**
      * Where to redirect users after login.
      *
